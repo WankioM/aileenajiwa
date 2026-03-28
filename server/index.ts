7de2c95301a678e6dbd91f81a14158866a849153
WankioM <tracywankio29@gmail.com>
2026-03-28 22:52:43 +0300

    cors

--- server/index.ts
+++ server/index.ts
@@ -10,7 +10,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://www.aileenowango.website"  // add your actual Vercel URL
+  ],
+}));
 app.use(express.json());
 
 // Routes
